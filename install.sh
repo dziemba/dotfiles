356b06c5cdf89b51c54db793de9d5eafe4253e44
Niko Dziemba <niko@dziemba.com>
2014-08-27 16:17:55 +0200

    add hostname setup to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 # sudo keepalive
 sudo -v
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
 
 # fix permissions if broken
 if [ -d /usr/local ]; then
@@ -10,6 +14,14 @@ if [ -d /usr/local ]; then
 fi
 
 cd $HOME/.dotfiles
+
+if [ ! -e .hostname_lock ]; then
+  echo -n "Enter hostname: "
+  read HOSTNAME
+  sudo scutil --set HostName $HOSTNAME
+  echo
+  touch .hostname_lock
+fi
 
 ./link.sh
 ./packages.sh
