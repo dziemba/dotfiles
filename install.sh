9cd6529cdec025684d07537362f81d53b9029da4
Niko Dziemba <niko@dziemba.com>
2014-08-27 15:17:34 +0200

    small fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+# sudo keepalive
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# fix permissions if broken
+if [ -d /usr/local ]; then
+  sudo chown $USER /usr/local
+fi
 
 cd $HOME/.dotfiles
 
