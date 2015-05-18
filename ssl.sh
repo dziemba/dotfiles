40c123edeed0137686f20a9c6079443c902cfe97
Niko Dziemba <niko@dziemba.com>
2015-05-18 11:20:44 +0200

    Use older (working) SSL CA bundle

--- ssl.sh
+++ ssl.sh
@@ -1,12 +1,6 @@
+CA_URL="https://raw.githubusercontent.com/bagder/ca-bundle/e9175fec/ca-bundle.crt"
 CERT_FILE=/usr/local/etc/openssl/cert.pem
+
 rm -f $CERT_FILE
-
-security find-certificate -a -p /Library/Keychains/System.keychain >> $CERT_FILE
-security find-certificate -a -p /System/Library/Keychains/SystemRootCertificates.keychain \
-  >> $CERT_FILE
-
-CA_URL="https://raw.githubusercontent.com/bagder/ca-bundle/master/ca-bundle.crt"
-curl $CA_URL >> $CERT_FILE
-
+curl $CA_URL > $CERT_FILE
 c_rehash /usr/local/etc/openssl
-
