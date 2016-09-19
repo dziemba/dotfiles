77f2a4bbbae7a819df4ea4bc157bf3c130a5ffa0
Niko Dziemba <niko@dziemba.com>
2016-09-19 11:48:09 +0200

    Do not re-install anaconda

--- packages.sh
+++ packages.sh
@@ -130,7 +130,7 @@ brew cask install flux
 brew cask install slack
 brew cask install docker
 brew cask install vagrant
-brew cask install anaconda
+[ -d ~/anaconda3 ] || brew cask install anaconda
 
 # Brew fonts
 brew tap caskroom/fonts
