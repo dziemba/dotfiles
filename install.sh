d6be6f37e41302309267277b0e5f9e1d2376ff2a
Niko Dziemba <niko@dziemba.com>
2017-02-09 13:17:53 +0100

    Remove broken sudo-keepalive and perm fix

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-# sudo keepalive
-sudo -v
-while true; do
-  sudo -n true
-  sleep 60
-  kill -0 "$$" || exit
-done 2>/dev/null &
-
-# fix permissions if broken
-if [ -d /usr/local ]; then
-  sudo chown $USER /usr/local
-fi
-
 cd $HOME/.dotfiles
 
 if [ ! -e .hostname_lock ]; then
