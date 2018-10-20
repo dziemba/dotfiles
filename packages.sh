1c08262c0a389a7962ead3433620c6872a54cfce
Niko Dziemba <niko@dziemba.com>
2018-10-20 16:34:41 +0200

    Use brew bundle

--- packages.sh
+++ packages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-# Brew
 if [ ! -f /usr/local/bin/brew ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -8,126 +7,14 @@ fi
 set -x
 
 brew update
-brew doctor || sleep 10
+brew bundle install
 brew upgrade
 
-brew tap caskroom/versions
-brew tap caskroom/fonts
-brew tap AdoptOpenJDK/openjdk
-
-# required for some brew packages
-brew cask install \
-  xquartz \
-  tuntap
-
-brew install \
-  adoptopenjdk-openjdk8 \
-  bash \
-  coreutils \
-  findutils \
-  bash-completion \
-  git \
-  gist \
-  hub \
-  gnu-sed \
-  go \
-  dep \
-  htop \
-  imagemagick \
-  lame \
-  mercurial \
-  nmap \
-  node \
-  openssl \
-  openvpn \
-  pngcrush \
-  rdesktop \
-  rename \
-  ruby \
-  sane-backends \
-  unrar \
-  vim \
-  watch \
-  wget \
-  wine \
-  postgresql \
-  redis \
-  direnv \
-  awscli \
-  mysql \
-  hiredis \
-  jq \
-  scala \
-  sbt \
-  elixir \
-  s3cmd \
-  wrk \
-  phantomjs \
-  llvm \
-  ansible \
-  markdown \
-  exiftool \
-  protobuf \
-  tidy-html5 \
-  gawk \
-  graphviz \
-  apache-spark \
-  binutils \
-  aria2 \
-  upx \
-  tree \
-  maven \
-  yarn \
-  vault \
-  docker \
-  docker-machine \
-  docker-machine-nfs \
-  docker-compose \
-  pwgen \
-  kafka \
-  ripgrep \
-  pdsh \
-  moreutils
-
-# Brew Fixes
-brew install openssl
-brew link openssl --force
-
-# Brew Cask
-brew cask install \
-  google-chrome \
-  iterm2 \
-  virtualbox \
-  dropbox \
-  libreoffice \
-  vlc \
-  transmission \
-  keka \
-  tor-browser \
-  rekordbox \
-  skype \
-  gimp \
-  firefox \
-  wireshark \
-  gpg-suite \
-  inkscape \
-  psequel \
-  keepingyouawake \
-  slack \
-  vagrant \
-  intellij-idea-ce \
-  spectacle \
-  kid3 \
-  sequel-pro \
-  font-source-code-pro-for-powerline
-
-# Brew cleanup
-brew prune
+brew bundle cleanup --zap --force
 brew cleanup
-brew doctor || sleep 10
+brew prune
 
-# Postinstall
-brew services start docker-machine
+brew doctor || sleep 10
 
 sudo chown root /usr/local/bin/htop
 sudo chmod +s /usr/local/bin/htop
